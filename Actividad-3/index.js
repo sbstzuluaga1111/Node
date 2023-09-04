d181e44c38986205eda5bc0d3b2c7db59927ac78
sbstzuluaga1111 <sbstzuluagajob@gmail.com>
2023-09-04 11:29:59 -0500

    V12

--- Actividad-3/index.js
+++ Actividad-3/index.js
@@ -1,17 +1,38 @@
 const fs = require('fs');
 const superagent = require('superagent')
 
-fs.readFile(`${__dirname}/dog.txt`,(err, data) => {
+const readFilePro = file => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if (err) reject('I could not find that file UnU')
+            resolve(data)
+        })
+    })
+}
+
+const writeFilePro = (file,data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file,data ,err => {
+        if (err) reject('Could not write file UnU')
+        resolve('success')
+        });
+    })
+}
+
+readFilePro(`${__dirname}/dog.txt`).then(data => {
     console.log(`Breed: ${data}`);
 
     superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) =>{
-        if(err) return console.log(err.message)
+    .then(res => {
         console.log(res.body.message);
 
         fs.writeFile('dog-img.txt',res.body.message, err =>{
+            if(err) return console.log(err.message);
             console.log('Rondom dog image saved to file!');
-        })
+        });
+    })
+    .catch(err => {
+        console.log(err.message)
     })
 })
